a6cb975f19bf5d8ad798fdca4995b7f391cfed3e
Jeisson Lazo <jeisson.lazo@apriorins.com>
2019-02-06 15:55:05 -0500

    commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+module.exports = {
+  module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [ 'style-loader', 'css-loader' ]
+            }
+        ],
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ],
+    output: {    
+        publicPath: '',
+    },
+}
